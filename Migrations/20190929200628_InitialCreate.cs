7b6b41af8004f6a09260acb59cf0647a9be1e9b7
Daniel Jara P <djara@corebox.cl>
2019-09-30 19:19:07 +0200

    articles model

---
+++ Migrations/20190929200628_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(nullable: true),
+                    text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
---
+++ Models/Articles.cs
@@ -0,0 +1,21 @@
+namespace api.Models
+{
+    public class Articles
+    {
+        public int id
+        {
+            get; set;
+        }
+
+        public string title
+        {
+            get; set;
+        }
+
+        public string text
+        {
+            get; set;
+        }
+    }
+}
+
