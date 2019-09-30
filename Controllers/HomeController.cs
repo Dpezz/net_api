dd81e0bae57f9242ef221c4fe910ef8b738f5305
Daniel Jara P <djara@corebox.cl>
2019-09-30 19:18:52 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
